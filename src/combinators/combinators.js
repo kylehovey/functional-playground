8b87855f8b470e945c4daf91b07777c59c5cba9b
khoveyc@gmail.com <khoveyc@gmail.com>
2019-03-06 22:03:12 -0700

    Added compose and repeated

--- src/combinators/combinators.js
+++ src/combinators/combinators.js
@@ -3,10 +3,24 @@
  * http://raganwald.com/2018/09/10/why-y.html
  */
 
+const compose = (...fns) => x => fns
+  .slice()
+  .reverse()
+  .reduce((acc, f) => f(acc), x);
+
+const repeated = (fn, n) => n !== 1
+  ? x => repeated(fn, n - 1)(fn(x))
+  : fn;
+
 const M = f => f(f);
 
 const Y = fn => M(
   maker => x => fn(maker(maker))(x)
 );
 
-module.exports = { M, Y };
+module.exports = {
+  M,
+  Y,
+  compose,
+  repeated
+};
--- tests/combinators.test.js
+++ tests/combinators.test.js
@@ -1,4 +1,33 @@
-const { M, Y } = require("../src/combinators/combinators.js");
+const {
+  M,
+  Y,
+  compose,
+  repeated,
+} = require("../src/combinators/combinators.js");
+
+describe("Composer", () => {
+  it("Composes a unary function", () => {
+    const square = x => x * x;
+
+    expect(compose(square, square)(2)).toBe(16);
+  });
+
+  it("Composes an arbitrary amount of functions in reverse order", () => {
+    const addSome = x => x + 8;
+    const multiply = x => x * 2;
+    const subtractSome = x => x - 3;
+
+    expect(compose(addSome, multiply, subtractSome)(2)).toBe(6);
+  });
+});
+
+describe("Repeated", () => {
+  it("Composes a function with itself an arbitrary amount of times", () => {
+    const collatz = n => (n % 2 === 0) ? (n / 2) : (3 * n + 1);
+
+    expect(repeated(collatz, 25)(625)).toBe(1);
+  });
+});
 
 describe("M Combinator", () => {
   it("Calls a function on itself", () => {
